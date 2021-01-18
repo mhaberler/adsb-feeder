ed5a30201e9be31410c7c61cee9686fd0b2c0d7a
Michael Haberler <git@mah.priv.at>
2021-01-18 19:04:17 +0100

    update client to current protocol and auth
    python client.py --url "wss://data.mah.priv.at/adsb/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c3IiOiJnaXRodWIiLCJkdXIiOjMwMCwiZXhwIjoxNjIyNTA1NjAwLjAsImlzcyI6InVybjptYWgucHJpdi5hdCIsImF1ZCI6WyJhZHNiLWdlb2J1ZiIsImFkc2ItanNvbiJdLCJpYXQiOjE2MTAwNjcwMTMsInJ1aSI6MzB9.jP4U5LPVw2BGRLQoy4oyEFosVYNfGxk2eTE_KqnTBTg" --protocol adsb-geobuf

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import sys
 import requests
+import geobuf
 import base64
 import json
 import argparse
@@ -33,52 +34,49 @@ class ADSBFeederClientProtocol(WebSocketClientProtocol):
         # after 2 seconds send a bbox update
         reactor.callLater(2, self.sendBBox)
 
+    def onConnect(self, response):
+        print(f"onConnect response={response} proto={self.websocket_protocol_in_use}")
 
     def onMessage(self, payload, isBinary):
-        if not isBinary:
+        if self.websocket_protocol_in_use == 'adsb-geobuf':
+            msg = geobuf.decode(payload)
+            print(f"geobuf received: isBinary={isBinary} msg={msg}")
+
+        if self.websocket_protocol_in_use == 'adsb-json':
             msg = json.loads(payload.decode('utf8'))
-            print(f"message received: {msg}")
+            print(f"json received: isBinary={isBinary} msg={msg}")
 
     def onClose(self, wasClean, code, reason):
-        print(f"WebSocket connection closed: {reason}")
+        print(f"WebSocket connection closed: wasClean={wasClean} code={code} reason={reason}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='adsb-feeder Python client example',
         add_help=True)
+
     parser.add_argument('--url',
                         dest='url',
                         action='store',
                         type=str,
                         required=True,
                         help='Websocket URL to connect to, like "wss://example.com/adsb/"')
-    parser.add_argument('--user',
-                        dest='user',
+
+    parser.add_argument('--protocol',
+                        dest='protocol',
                         action='store',
                         type=str,
-                        required=True,
-                        help='username')
-    parser.add_argument('--password',
-                        dest='password',
-                        action='store',
-                        type=str,
-                        required=True,
-                        help='password')
+                        default=None,
+                        help="Websocket subprotocol to use, like adsb-geobuf or adsb-json")
 
     args = parser.parse_args()
-
-    # use Basic Authentication
-    usrPass = f"{args.user}:{args.password}"
-    b64val = base64.b64encode(usrPass.encode()).decode("utf8")
-    headers={"Authorization": f"Basic {b64val}"}
-
-    # create a WS server factory with our protocol
-    ##
+    headers = []
     factory = WebSocketClientFactory(args.url,  headers=headers)
     factory.protocol = ADSBFeederClientProtocol
+    if args.protocol:
+        factory.protocols = [args.protocol]
+    else:
+        factory.protocols = ['adsb-geobuf', 'adsb-json']
 
-    # SSL client context: default
-    ##
     if factory.isSecure:
         contextFactory = ssl.ClientContextFactory()
     else:
